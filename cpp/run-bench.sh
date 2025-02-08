367d5920128b3b11dccbadb4de0024a24e96aee1
stevenybw <stevenybw@hotmail.com>
2025-02-08 13:25:44 +0000

    bench script

--- cpp/run-bench.sh
+++ cpp/run-bench.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-for NAME in $(find . -maxdepth 1 -type f -executable | xargs -i basename {}); do
+INPUT_DIR=build/benchmarks
+OUTPUT_DIR=bench_output/g4dn.xlarge
+
+mkdir -p $OUTPUT_DIR
+
+for NAME in $(find $INPUT_DIR -maxdepth 1 -type f -executable | xargs -i basename {}); do
     echo $NAME
+    $INPUT_DIR/$NAME 1>$OUTPUT_DIR/bench_${NAME}.stdout 2>$OUTPUT_DIR/bench_${NAME}.stderr
 done
